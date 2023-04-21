f3d156cbe9f28440fbc3e4c26802c0be6c65b672
Sairoden <odingandarosa@gmail.com>
2023-04-21 11:43:43 +0800

    fix production bug v2

--- src/components/Context/UserContext.js
+++ src/components/Context/UserContext.js
@@ -1,3 +1,58 @@
+// import { useEffect } from "react";
+// import { useState, createContext } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// export const UserContext = createContext({
+//   user: false,
+//   firstName: "",
+//   lastName: "",
+// });
+
+// export const UserProvider = ({ children }) => {
+//   const [user, setUser] = useState(false);
+//   const [firstName, setFirstName] = useState("");
+//   const [lastName, setLastName] = useState("");
+//   const navigate = useNavigate();
+
+//   const logout = () => {
+//     setUser(false);
+//     setFirstName("");
+//     setLastName("");
+//     localStorage.setItem("items", JSON.stringify(null));
+//     navigate("/");
+//   };
+
+//   if (firstName && lastName && user) {
+//     const items = {
+//       firstName,
+//       lastName,
+//       user,
+//     };
+//     localStorage.setItem("items", JSON.stringify(items));
+//   }
+
+//   useEffect(() => {
+//     const items = JSON.parse(localStorage.getItem("items"));
+
+//     if (items) {
+//       setFirstName(items.firstName);
+//       setLastName(items.lastName);
+//       setUser(true);
+//     }
+//   }, [user]);
+
+//   const value = {
+//     user,
+//     setUser,
+//     firstName,
+//     lastName,
+//     setFirstName,
+//     setLastName,
+//     logout,
+//   };
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// };
+
 import { useEffect } from "react";
 import { useState, createContext } from "react";
 import { useNavigate } from "react-router-dom";
@@ -12,33 +67,11 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(false);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+
   const navigate = useNavigate();
 
-  const logout = () => {
-    setUser(false);
-    setFirstName("");
-    setLastName("");
-    localStorage.setItem("items", JSON.stringify(null));
-    navigate("/");
-  };
-
-  if (firstName && lastName && user) {
-    const items = {
-      firstName,
-      lastName,
-      user,
-    };
-    localStorage.setItem("items", JSON.stringify(items));
-  }
-
   useEffect(() => {
-    const items = JSON.parse(localStorage.getItem("items"));
-
-    if (items) {
-      setFirstName(items.firstName);
-      setLastName(items.lastName);
-      setUser(true);
-    }
+    navigate("/");
   }, [user]);
 
   const value = {
@@ -48,7 +81,6 @@ export const UserProvider = ({ children }) => {
     lastName,
     setFirstName,
     setLastName,
-    logout,
   };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
